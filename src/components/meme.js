9b9628719b6742756e75b691ce4e869793695ae1
AdaN6 <a_disney_da@hotmail.com>
2022-11-07 11:52:17 +0100

    random

--- src/components/meme.js
+++ src/components/meme.js
@@ -5,12 +5,27 @@ export const Meme = () => {
   const [memes, setMemes] = useState([]);
   const [memeIndex, setMemeIndex] = useState(0);
 
+
+  const shuffleMemes = (array) => {
+  
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i);
+  
+      const temp = array[i];
+      array[i] = array[j];
+      array [j] = temp;
+    }
+  }
+
+  
+
   useEffect(() => {
     fetch("https://api.imgflip.com/get_memes")
       .then((res) => res.json())
       .then((res) => {
-        const memes = res.data.memes;
-        setMemes(memes);
+        const _memes = res.data.memes;
+        shuffleMemes(_memes);
+        setMemes(_memes);
       });
   }, []);
 
