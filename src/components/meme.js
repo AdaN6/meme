5896c7946038fb8cda22fef4868712dbcbadc1f3
AdaN6 <a_disney_da@hotmail.com>
2022-11-13 14:37:39 +0100

    .

--- src/components/meme.js
+++ src/components/meme.js
@@ -4,13 +4,13 @@ import Styles from "./styles.module.css";
 export const Meme = () => {
   const [memes, setMemes] = useState([]);
   const [memeIndex, setMemeIndex] = useState(0);
+  const [captions, setCaptions] = useState([]);
 
 
   const shuffleMemes = (array) => {
   
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * i);
-  
       const temp = array[i];
       array[i] = array[j];
       array [j] = temp;
@@ -29,8 +29,20 @@ export const Meme = () => {
       });
   }, []);
 
+  useEffect(()=> {
+if (memes.length) {
+
+}
+  }, [memeIndex, memes])
+
   return memes.length ? (
     <div className={Styles.container}>
+      <button
+        onClick={() => console.log('generate')}
+        className={Styles.generate}
+      >
+        Generate
+      </button>
       <button
         onClick={() => setMemeIndex(memeIndex + 1)}
         className={Styles.next}
